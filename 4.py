7b180e7a908e67c8246fc90a517bc83b34628a69
grff5 <127615981+grff5@users.noreply.github.com>
2024-02-14 17:55:43 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,37 @@
+import csv
+
+def hash(s):
+    alp = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890:-'
+    m = 10**9 + 9
+    p = 65
+    d = {}
+    for ind, symbol in enumerate(alp, 1):
+        d[symbol] = ind
+
+    r = 0
+    for i in range(len(s)):
+        r += int(d[s[1]])*(p**i)
+    return str(r % m)
+
+'''
+Считывание файла в двумерный массив
+'''
+f = open('game.txt', encoding = 'utf-8')
+data=[x.strip().split('$') for x in f]
+f.close()
+
+list = []
+
+for i in range(1, len(data)):
+    s = data[i][0]+data[i][1]
+    s = hash(s)
+    list.append([s, data[i][0], data[i][2]])
+
+
+'''
+Создание нового scv файла
+'''
+with open('game_with_hash.csv', 'w', encoding = 'utf-8') as file:
+    w = csv.writer(file)
+    w.writerows(['hash', 'GameName', 'characters'])
+    w.writerows(list)
